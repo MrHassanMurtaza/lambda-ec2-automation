1013206062621472dc098d0dba99c9a39061238f
Hassan Murtaza <mrhassanmurtaza@gmail.com>
2019-04-13 20:51:42 +0500

    Adding EC2 Stop Script

---
+++ ec2-stop/index.js
@@ -0,0 +1,22 @@
+
+const AWS = require('aws-sdk')
+let fs = require("fs");
+
+
+exports.handler = (event, context, callback) => {
+    
+    console.log(`Region ${event.instanceRegion}`);
+    const ec2 = new AWS.EC2({ region: event.instanceRegion });
+    var array = fs.readFileSync(__dirname + "/instanceIds.txt").toString().split('\n');
+    // var InstanceKeyVal = {}
+    var listOfInstances = []
+    array.forEach(function(instanceId){
+        if(instanceId){
+            listOfInstances.push(instanceId)
+        }
+    })
+    
+    ec2.stopInstances({ InstanceIds: listOfInstances }).promise()
+        .then((data) => console.log(data) )
+        .catch(err => callback(err));
+};
